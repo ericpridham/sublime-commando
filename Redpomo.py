480dc5f14f7ab2eb986ef4c75944dba48f0ea8e9
Eric Pridham <epridham@replicocorp.com>
2013-04-29 13:26:45 -0700

    More stuff!

--- Redpomo.py
+++ Redpomo.py
@@ -1,13 +1,13 @@
 import sublime, sublime_plugin
 import Commando.Core as CC
 import re
+import webbrowser
+import time
 
 class RedpomoCommand(CC.Command, sublime_plugin.WindowCommand):
   priority_descs = {'A':'Immediate','B':'Urgent','C':'High'}
-  actions = [
-    {'action': 'open',  'desc': 'Open'},
-    {'action': 'start', 'desc': 'Start'},
-    {'action': 'close', 'desc': 'Close'}]
+  actions = ['open', 'start', 'close']
+  action_descs = ['Open', 'Start', 'Close']
 
   def redpomo(self, params, callback = None):
     s = sublime.load_settings("Redpomo.sublime-settings")
@@ -20,6 +20,33 @@ class RedpomoCommand(CC.Command, sublime_plugin.WindowCommand):
     cmd = s.get('todo_command')
     if cmd:
       self.exec_command(cmd, params, callback)
+
+  #Issue created, see it at http://projects.replicocorp.com/issues/#
+  def open_issue_url(self, output):
+    p = output.find("http://")
+    if p != -1:
+      webbrowser.open(output[p:].strip()+'/edit', 2)
+    else:
+      self.generic_done(output)
+
+class RedpomoPullCommand(RedpomoCommand):
+  def run(self):
+    self.redpomo(['pull'], self.pull_done)
+
+  def pull_done(self, output):
+    self.window.run_command('redpomo_list')
+
+class RedpomoAddCommand(RedpomoCommand):
+  def run(self):
+    self.prompt("redpomo add", "", self.on_input, None, None)
+
+  def on_input(self, new_task):
+    if not '@' in new_task:
+      s = sublime.load_settings("redpomo.sublime-settings")
+      default_tracker = s.get('default_tracker')
+      if default_tracker:
+        new_task += " @"+default_tracker
+    self.redpomo(['add', new_task], self.open_issue_url)
 
 class RedpomoListCommand(RedpomoCommand):
   def run(self):
@@ -44,15 +71,47 @@ class RedpomoListCommand(RedpomoCommand):
     else:
       task_list = [["{issue} {subject}".format(**t), "{priority_desc} {project}".format(**t)] for t in self.tasks]
       pass
-    self.select(task_list, self.ticket_selected, sublime.MONOSPACE_FONT)
+    self.select(task_list, self.get_task_num, sublime.MONOSPACE_FONT)
 
-  def ticket_selected(self, selected):
+  def get_task_num(self, selected):
     if 0 <= selected < len(self.tasks):
-      self.panel("{issue} {subject}".format(**self.tasks[selected]))
+      self.do_task_action(self.tasks[selected]['issue'])
 
-class RedpomoPullCommand(RedpomoCommand):
-  def run(self):
-    self.redpomo(['pull'], self.pull_done)
+  def do_redpomo_action(self, action, task_num):
+    if action == 'open':
+      self.redpomo(['open', task_num])
+    elif action == 'start':
+      self.redpomo(['open', task_num])
+      time.sleep(1)
+      self.redpomo(['start', task_num])
+    elif action == 'close':
+      self.task_num_selected = task_num
+      self.prompt("redpomo close", "", self.on_redpomo_close_input, None, None)
 
-  def pull_done(self, output):
-    self.window.run_command('redpomo_list')
+  def on_redpomo_close_input(self, message):
+    if message.strip():
+      self.redpomo(['close', '-m', message.strip(), self.task_num_selected], self.open_issue_url)
+
+  #
+  # Override from here down
+  #
+  def do_task_action(self, task_num):
+    self.task_num_selected = task_num
+    self.select(self.action_descs, self.get_command)
+
+  def get_command(self, selected):
+    if 0 <= selected < len(self.actions):
+      self.do_redpomo_action(self.actions[selected], self.task_num_selected)
+
+
+class RedpomoOpenCommand(RedpomoListCommand):
+  def do_task_action(self, task_num):
+    self.do_redpomo_action('open', task_num)
+
+class RedpomoStartCommand(RedpomoListCommand):
+  def do_task_action(self, task_num):
+    self.do_redpomo_action('start', task_num)
+
+class RedpomoCloseCommand(RedpomoListCommand):
+  def do_task_action(self, task_num):
+    self.do_redpomo_action('close', task_num)
