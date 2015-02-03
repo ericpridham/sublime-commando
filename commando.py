971d7a739b4dc210dc946ca1a0de548fc0680ac7
Eric Pridham <me@ericpridham.com>
2015-02-02 21:41:08 -0800

    Copied Default.exec classes natively so I can begin to add input support to AsyncProcess

--- commando.py
+++ commando.py
@@ -28,11 +28,129 @@ import collections
 import pipes
 import string
 
-from Default.exec import ProcessListener, AsyncProcess
+#from Default.exec import ProcessListener, AsyncProcess
+####
+####
+####
+class ProcessListener(object):
+  def on_data(self, proc, data):
+    pass
+
+  def on_finished(self, proc):
+    pass
+
+# Encapsulates subprocess.Popen, forwarding stdout to a supplied
+# ProcessListener (on a separate thread)
+class AsyncProcess(object):
+  def __init__(self, cmd, shell_cmd, env, listener, path="", shell=False):
+
+    if not shell_cmd and not cmd:
+      raise ValueError("shell_cmd or cmd is required")
+
+    if shell_cmd and not isinstance(shell_cmd, str):
+      raise ValueError("shell_cmd must be a string")
+
+    self.listener = listener
+    self.killed = False
+
+    self.start_time = time.time()
+
+    # Hide the console window on Windows
+    startupinfo = None
+    if os.name == "nt":
+      startupinfo = subprocess.STARTUPINFO()
+      startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+    # Set temporary PATH to locate executable in cmd
+    if path:
+      old_path = os.environ["PATH"]
+      # The user decides in the build system whether he wants to append $PATH
+      # or tuck it at the front: "$PATH;C:\\new\\path", "C:\\new\\path;$PATH"
+      os.environ["PATH"] = os.path.expandvars(path)
+
+    proc_env = os.environ.copy()
+    proc_env.update(env)
+    for k, v in proc_env.items():
+      proc_env[k] = os.path.expandvars(v)
+
+    if shell_cmd and sys.platform == "win32":
+      # Use shell=True on Windows, so shell_cmd is passed through with the correct escaping
+      self.proc = subprocess.Popen(shell_cmd, stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE, startupinfo=startupinfo, env=proc_env, shell=True)
+    elif shell_cmd and sys.platform == "darwin":
+      # Use a login shell on OSX, otherwise the users expected env vars won't be setup
+      self.proc = subprocess.Popen(["/bin/bash", "-l", "-c", shell_cmd], stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE, startupinfo=startupinfo, env=proc_env, shell=False)
+    elif shell_cmd and sys.platform == "linux":
+      # Explicitly use /bin/bash on Linux, to keep Linux and OSX as
+      # similar as possible. A login shell is explicitly not used for
+      # linux, as it's not required
+      self.proc = subprocess.Popen(["/bin/bash", "-c", shell_cmd], stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE, startupinfo=startupinfo, env=proc_env, shell=False)
+    else:
+      # Old style build system, just do what it asks
+      self.proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE, startupinfo=startupinfo, env=proc_env, shell=shell)
+
+    if path:
+      os.environ["PATH"] = old_path
+
+    if self.proc.stdout:
+      threading.Thread(target=self.read_stdout).start()
+
+    if self.proc.stderr:
+      threading.Thread(target=self.read_stderr).start()
+
+  def kill(self):
+    if not self.killed:
+      self.killed = True
+      if sys.platform == "win32":
+        # terminate would not kill process opened by the shell cmd.exe, it will only kill
+        # cmd.exe leaving the child running
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        subprocess.Popen("taskkill /PID " + str(self.proc.pid), startupinfo=startupinfo)
+      else:
+        self.proc.terminate()
+      self.listener = None
+
+  def poll(self):
+    return self.proc.poll() == None
+
+  def exit_code(self):
+    return self.proc.poll()
+
+  def read_stdout(self):
+    while True:
+      data = os.read(self.proc.stdout.fileno(), 2**15)
+
+      if len(data) > 0:
+        if self.listener:
+          self.listener.on_data(self, data)
+      else:
+        self.proc.stdout.close()
+        if self.listener:
+          self.listener.on_finished(self)
+        break
+
+  def read_stderr(self):
+    while True:
+      data = os.read(self.proc.stderr.fileno(), 2**15)
+
+      if len(data) > 0:
+        if self.listener:
+          self.listener.on_data(self, data)
+      else:
+        self.proc.stderr.close()
+        break
+###
+###
+###
 
 class CommandoKillCommand(sublime_plugin.WindowCommand):
   def run(self):
     sublime.run_command("commando_exec", {"kill": True})
+
 #
 # Module functions
 #
@@ -42,14 +160,14 @@ def class_to_command(cls):
   name = clsname[0].lower()
   last_upper = False
   for c in clsname[1:]:
-      if c.isupper() and not last_upper:
-          name += '_'
-          name += c.lower()
-      else:
-          name += c
-      last_upper = c.isupper()
+    if c.isupper() and not last_upper:
+      name += '_'
+      name += c.lower()
+    else:
+      name += c
+    last_upper = c.isupper()
   if name.endswith("_command"):
-      name = name[0:-8]
+    name = name[0:-8]
   return name
 
 def get_command_type(command):
@@ -241,15 +359,15 @@ class Commando:
   def get_working_dir(self):
     context = self.get_context()
     if context:
-      if context['window_id'] and context['view_id']:
+      if isinstance(self, sublime_plugin.TextCommand):
         view = get_view_by_id(context['window_id'], context['view_id'])
         if view.file_name():
           return os.path.dirname(view.file_name())
-
-      elif context['window_id']:
+      else:
         window = get_window_by_id(context['window_id'])
         if window.folders():
           return window.folders()[0]
+    return None
 
   def get_filename(self):
     view = get_view_by_context(self.get_context())
@@ -261,7 +379,6 @@ class Commando:
       window = get_window_by_id(context['window_id'])
       if window.folders():
         return window.folders()[0] + ('/' + filename if filename else '')
-
 
     return None
 
@@ -312,7 +429,6 @@ class CommandoExecCommand(ApplicationCommando, ProcessListener):
   longrun = False
 
   def cmd(self, input, args):
-
     if not 'cmd' in args:
       return
 
@@ -343,6 +459,7 @@ class CommandoExecCommand(ApplicationCommando, ProcessListener):
     if 'working_dir' in args:
       working_dir = args['working_dir']
     else:
+      print(self.get_context())
       working_dir = self.get_working_dir()
 
     # Change to the working dir, rather than spawning the process with it,
@@ -449,12 +566,12 @@ class CommandoNewFileCommand(ApplicationCommando):
         ro = args['ro']
       if 'syntax' in args:
         syntax = args['syntax']
-      new_file(content.rstrip(), self.get_context(), name=name, scratch=scratch, ro=ro, syntax=syntax)
+      new_file(input.rstrip(), self.get_context(), name=name, scratch=scratch, ro=ro, syntax=syntax)
 
 class CommandoOpenFileCommand(ApplicationCommando):
-  def cmd(self, input):
+  def cmd(self, input, args):
     if os.path.exists(input):
-      new_file(content.rstrip(), self.get_context(), name=name, scratch=scratch, ro=ro, syntax=syntax)
+      open_file(input, self.get_context())
 
 class CommandoSelectCommand(ApplicationCommando):
   def cmd(self, input, args):#on_done=None):
